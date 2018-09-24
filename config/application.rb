261ece47618ae29dd73f51f792fcc66421c2a432
Ho Sum Cheung <samcheung@Hos-MacBook-Pro.local>
2018-09-24 13:57:43 +1200

    changed coffee script

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'coffee-rails'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +11,8 @@ module JobNotes
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # commit change and redeploy to Heroku without app attempts to connect to the database when compiling asset
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
